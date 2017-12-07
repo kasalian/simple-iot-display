08c5247158b5eba57f45810f66ea98cc5e1bcd4c
Chinaka Light <chinakalight@googlemail.com>
2017-12-07 11:10:11 +0100

    updated the dbconnect and index

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,11 @@
 <?php 
+
 	$DB_HOST = "sql3.freemysqlhosting.net";
 	$DB_NAME = "sql3209328";
 	$DB_USERNAME = "sql3209328";
 	$DB_PASSWORD = "CeKYrG2Wrl";
 	$DB_PORT = "3306";
+
 
 	$myPDO = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USERNAME, $DB_PASSWORD);
 
