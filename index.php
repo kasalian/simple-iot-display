7dc5734ac3d86a37867c47e264eb3c440a74eb87
Chinaka Light <chinakalight@googlemail.com>
2017-12-08 19:11:48 +0100

    added chart library as well as updated the index.php to reflect it

--- index.php
+++ index.php
@@ -11,9 +11,13 @@
 			text-align: center;
 		}
 	</style>
+	<script src="dist/Chart.bundle.min.js"></script>
+    <!-- <script src="dist/utils.js"></script> -->
 </head>
 <body>
 	<h1 id='title-h1'>GARBAGE LEVEL INDICATOR</h1><br/>
+
+	<div style="float:left; width: 25em;background-color: white">
 		<?php
 
 	require('dbconnect.php');
@@ -33,8 +37,57 @@
 	}
 
 
-?>
+?>	
+	</div>
+	<div  style="float:left; width: 50em; background-color: lime">
+		<canvas id="bar-chart" width="800" height="450"></canvas>
 
+
+	</div>
+	    <script>
+        
+        window.onload = function() {
+            // Bar chart
+				new Chart(document.getElementById("bar-chart"), {
+				    type: 'bar',
+				    data: {
+				      labels: ["Bauchi"],
+				      datasets: [
+				        {
+				          label: "Filled Level (Cm)",
+				          backgroundColor: ["#3e95cd", "#ee2323"],
+				          data: [65.8]
+				        }
+				      ]
+				    },
+				    options: {
+				      legend: { display: false },
+				      title: {
+				        display: true,
+				        text: 'Garbage Level Indicator'
+				      }
+				    }
+				});
+
+
+
+        };
+/*
+
+data: {
+				      labels: ["Bauchi", "Asia", "Europe", "Latin America", "North America"],
+				      datasets: [
+				        {
+				          label: "Population (millions)",
+				          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+				          data: [70,60,55,30,20]
+				        }
+				      ]
+				    },
+
+ */
+      
+    </script>
 
 </body>
 </html>
