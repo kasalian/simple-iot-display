64a3140d567a715249b8fb499ec5391344b67bd0
Chinaka Light <chinakalight@googlemail.com>
2017-12-08 04:44:39 +0100

    added the composer and its lock file as well as the dbconnect and index

--- dbconnect.php
+++ dbconnect.php
@@ -1,10 +1,21 @@
 <?php 
 
-	$DB_HOST = getenv('M_DB_HOST');
-	$DB_NAME = getenv('M_DB_NAME');
-	$DB_USERNAME = getenv('M_DB_USERNAME');
-	$DB_PASSWORD = getenv('M_DB_PASSWORD');
-	$DB_PORT = "3306";
+	
+	require __DIR__ . '/vendor/autoload.php';
+
+	echo "First: " . getenv("M_DB_HOST");
+
+	$dotenv = new Dotenv\Dotenv(__DIR__);
+	$dotenv->load();
+
+
+	echo "<br/>SECOND: " . getenv("M_DB_HOST");
+
+	$DB_HOST = getenv("M_DB_HOST");
+	$DB_NAME = getenv("M_DB_NAME");
+	$DB_USERNAME = getenv("M_DB_USERNAME");
+	$DB_PASSWORD = getenv("M_DB_PASSWORD");
+	$DB_PORT = getenv("M_DB_PORT");
 
 
 	$myPDO = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USERNAME, $DB_PASSWORD);
