c06ccd177a57d8d2c7090c2ea766bf6e6dd5d2a6
Chinaka Light <chinakalight@googlemail.com>
2017-12-08 03:40:16 +0100

    walkaround to be able to access heroku .env variable while I also use the local .env variable also

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,7 @@
 <?php 
 
-	if(file_exists(__DIR__ . '/vendor/autoload.php')){
+	// if it is my local server and not heroku hosted server
+	if(stristr($_SERVER['HTTP_HOST'], 'local') || (substr($_SERVER['HTTP_HOST'], 0, 7) == '192.168')){
 		require __DIR__ . '/vendor/autoload.php';
 
 
