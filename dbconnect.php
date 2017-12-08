40fcd8637c1c3cb23dc3272ae496968761684812
Chinaka Light <chinakalight@googlemail.com>
2017-12-08 03:56:46 +0100

    fix bug not displaying e

--- dbconnect.php
+++ dbconnect.php
@@ -1,20 +1,19 @@
 <?php 
 
 	// require __DIR__ . '/vendor/autoload.php';
-	
+
 	// if it is my local server and not heroku hosted server
-	if(stristr($_SERVER['HTTP_HOST'], 'local') || (substr($_SERVER['HTTP_HOST'], 0, 7) == '192.168')){
-
-
-		// $dotenv = new Dotenv\Dotenv(__DIR__);
-		// $dotenv->load();
-	}
+	
+	// $dotenv = new Dotenv\Dotenv(__DIR__);
+	// $dotenv->load();
 
 	$DB_HOST = getenv("M_DB_HOST");
 	$DB_NAME = getenv("M_DB_NAME");
 	$DB_USERNAME = getenv("M_DB_USERNAME");
 	$DB_PASSWORD = getenv("M_DB_PASSWORD");
 	$DB_PORT = getenv("M_DB_PORT");
+
+	echo "$DB_HOST $DB_NAME $DB_USERNAME $DB_PASSWORD $DB_PORT";
 
 
 	$myPDO = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USERNAME, $DB_PASSWORD);
