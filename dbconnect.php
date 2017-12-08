4d285e27efe7bf0aa68e3b9ad003bf9fd606e6a9
Chinaka Light <chinakalight@googlemail.com>
2017-12-08 04:39:10 +0100

    updated the naming for the env

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php 
 
-	$DB_HOST = "sql3.freemysqlhosting.net";
-	$DB_NAME = "sql3209328";
-	$DB_USERNAME = "sql3209328";
-	$DB_PASSWORD = "CeKYrG2Wrl";
+	$DB_HOST = getenv('M_DB_HOST');
+	$DB_NAME = getenv('M_DB_NAME');
+	$DB_USERNAME = getenv('M_DB_USERNAME');
+	$DB_PASSWORD = getenv('M_DB_PASSWORD');
 	$DB_PORT = "3306";
 
 
