1ebce80285b538607015f88ca183ae3c41cee864
Chinaka Light <chinakalight@googlemail.com>
2017-12-08 03:50:47 +0100

    fix bug not displaying c

--- dbconnect.php
+++ dbconnect.php
@@ -6,8 +6,8 @@
 	if(stristr($_SERVER['HTTP_HOST'], 'local') || (substr($_SERVER['HTTP_HOST'], 0, 7) == '192.168')){
 
 
-		$dotenv = new Dotenv\Dotenv(__DIR__);
-		$dotenv->load();
+		// $dotenv = new Dotenv\Dotenv(__DIR__);
+		// $dotenv->load();
 	}
 
 	$DB_HOST = getenv("M_DB_HOST");
