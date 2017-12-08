c088b7bfe2e01998c54ff1859b9aa644851695d8
Chinaka Light <chinakalight@googlemail.com>
2017-12-08 04:46:59 +0100

    quick fix

--- dbconnect.php
+++ dbconnect.php
@@ -1,15 +1,15 @@
 <?php 
 
 	
-	require __DIR__ . '/vendor/autoload.php';
+	// require __DIR__ . '/vendor/autoload.php';
 
-	echo "First: " . getenv("M_DB_HOST");
+	// echo "First: " . getenv("M_DB_HOST");
 
-	$dotenv = new Dotenv\Dotenv(__DIR__);
-	$dotenv->load();
+	// $dotenv = new Dotenv\Dotenv(__DIR__);
+	// $dotenv->load();
 
 
-	echo "<br/>SECOND: " . getenv("M_DB_HOST");
+	// echo "<br/>SECOND: " . getenv("M_DB_HOST");
 
 	$DB_HOST = getenv("M_DB_HOST");
 	$DB_NAME = getenv("M_DB_NAME");
