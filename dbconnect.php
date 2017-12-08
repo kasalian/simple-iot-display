47eba6e40701f2742012aa83678b538ede974837
Chinaka Light <chinakalight@googlemail.com>
2017-12-08 03:49:24 +0100

    fix bug not displaying b

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,9 @@
 <?php 
 
+	require __DIR__ . '/vendor/autoload.php';
+	
 	// if it is my local server and not heroku hosted server
 	if(stristr($_SERVER['HTTP_HOST'], 'local') || (substr($_SERVER['HTTP_HOST'], 0, 7) == '192.168')){
-		require __DIR__ . '/vendor/autoload.php';
 
 
 		$dotenv = new Dotenv\Dotenv(__DIR__);
